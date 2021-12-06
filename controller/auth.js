20f855348b49d7dc900d01dffcbc2729156a9cf6
JosephGabriel <josegabriel.freelancer@gmail.com>
2021-12-06 18:36:37 -0300

    Função de criar usúario criada

---
+++ controller/auth.js
@@ -0,0 +1,50 @@
+const { promisify } = require('util');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signupToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendUser = (user, statusCode, res) => {
+  const token = signupToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  user.password = undefined;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  createSendUser(newUser, 200, res);
+});
